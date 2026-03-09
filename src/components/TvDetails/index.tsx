3e3eb77c5f5529d9a7a21c55ce860df25b5790df
RAFSuNX <git@rafsunx.com>
2026-03-09 12:26:00 +0600

    fix(details): restore desktop crew spacing

--- src/components/TvDetails/index.tsx
+++ src/components/TvDetails/index.tsx
@@ -1674,7 +1674,7 @@ const TvDetails = ({ tv }: TvDetailsProps) => {
             {data.overview || intl.formatMessage(messages.overviewunavailable)}
           </p>
           {sortedCrew.length > 0 && (
-            <div className="relative -mt-[50px] pt-6">
+            <div className="relative -mt-[50px] pt-6 sm:mt-3">
               <div className="absolute inset-x-0 top-0 border-t border-white/[0.12]" />
               <div className="relative">
                 <div className="flex items-center justify-between mb-3">
