b942c0276b76c3c1679fe34d6d02f1f9c2126dc1
RAFSuNX <git@rafsunx.com>
2026-03-09 12:11:02 +0600

    fix(tv): cap episode text width beside season image

--- src/components/TvDetails/Season/index.tsx
+++ src/components/TvDetails/Season/index.tsx
@@ -44,7 +44,7 @@ const Season = ({ seasonNumber, tvId }: SeasonProps) => {
                 className="flex flex-col space-y-4 py-4 xl:flex-row xl:space-x-4 xl:space-y-4"
                 key={`season-${seasonNumber}-episode-${episode.episodeNumber}`}
               >
-                <div className="xl:min-w-0">
+                <div className="xl:min-w-0 xl:max-w-3xl">
                   <div className="flex flex-col space-y-2 xl:flex-row xl:items-center xl:space-x-2 xl:space-y-0">
                     <h3 className="text-lg">
                       {episode.episodeNumber} - {episode.name}
